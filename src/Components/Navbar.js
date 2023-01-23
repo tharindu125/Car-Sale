8bfa4e29d62fd8838239724242647bc155fa51be
tharindu125 <nawanjanaenda@gmail.com>
2023-01-23 20:54:19 +0530

    navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar() {
     <Router>
     <div>
 
-        <nav class="navbar navbar-expand-md bg-light" role="navigation">
+        {/* <nav class="navbar navbar-expand-md bg-light" role="navigation">
 
             <a class="navbar-brand" href="./">
                 <img src="logo.png" width="70" height="50" alt="" style={{marginLeft:'30px'}}/>
@@ -55,6 +55,43 @@ export default function Navbar() {
 
                 </form>
             </div>
+        </nav> */}
+
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+
+            <a class="navbar-brand" href="./">
+                <img src="logo.png" width="70" height="50" alt="" style={{marginLeft:'30px'}}/>
+            </a>
+            <a class="navbar-brand" href=".#">Star Car Sale</a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+
+                <ul class="navbar-nav">
+
+                <li class="nav-item active">
+                    <a class="nav-link" href=".#">Home <span class="sr-only">(current)</span></a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href=".#">Features</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href=".#">Pricing</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link disabled" href=".#">Disabled</a>
+                </li>
+
+                </ul>
+
+            </div>
+
         </nav>
 
         <Routes>
