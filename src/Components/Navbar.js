76fed5b6d214d85d9b3cd1a29aa1062df6c2699f
tharindu125 <nawanjanaenda@gmail.com>
2023-01-24 20:19:27 +0530

    edit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,60 +9,12 @@ export default function Navbar() {
     <Router>
     <div>
 
-        {/* <nav class="navbar navbar-expand-md bg-light" role="navigation">
+        <nav class="navbar navbar-expand-md navbar-light bg-light">
 
             <a class="navbar-brand" href="./">
                 <img src="logo.png" width="70" height="50" alt="" style={{marginLeft:'30px'}}/>
             </a>
-
-            <a class="navbar-brand " href="./" style={{fontSize:'30px', fontFamily:'Lucida Sans Unicode'}}>Star Car Sale</a>
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-                <ul class="navbar-nav mr-auto" >
-
-                    <li class="nav-item active">
-                        <a class="nav-link" href="./Signin.js" >Sign In <span class="sr-only">(current)</span></a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="./Login.js">LogIn</a>
-                    </li>
-                    
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href=".#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href=".#">Action</a>
-                            <a class="dropdown-item" href=".#">Another action</a>
-
-                        <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href=".#">Something else here</a>
-                        </div>
-                    </li>
-
-                </ul>
-
-                <form class="form-inline my-2 my-lg-1">
-
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-
-                </form>
-            </div>
-        </nav> */}
-
-        <nav class="navbar navbar-expand-sm navbar-light bg-light">
-
-            <a class="navbar-brand" href="./">
-                <img src="logo.png" width="70" height="50" alt="" style={{marginLeft:'30px'}}/>
-            </a>
-            <a class="navbar-brand" href=".#">Star Car Sale</a>
+            <a class="navbar-brand" href="./" style={{fontSize:'30px', fontFamily:'Lucida Sans Unicode'}}>Star Car Sale <span class="sr-only">(current)</span> </a>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -72,26 +24,20 @@ export default function Navbar() {
 
                 <ul class="navbar-nav">
 
-                <li class="nav-item active">
-                    <a class="nav-link" href=".#">Home <span class="sr-only">(current)</span></a>
-                </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="./">Catogeries </a>
+                    </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href=".#">Features</a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./Signin.js">Signin</a>
+                    </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href=".#">Pricing</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link disabled" href=".#">Disabled</a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./Login.js">Login</a>
+                    </li>
 
                 </ul>
-
             </div>
-
         </nav>
 
         <Routes>
--- src/Components/Signin.js
+++ src/Components/Signin.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 export default function Signin() {
     return(
-        <div>
-            sigin
+        <div className='container '>
+            
+            
+            <button type='button' className='btn btn-outline-primary btn-center'> Register as buyer</button>
+            <br/>
+            <button type='button' className='btn btn-outline-primary btn-center'> Register as seller</button>
+
         </div>
     )
 }
